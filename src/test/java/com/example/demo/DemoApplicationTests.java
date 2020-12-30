a8717c2c1b60d8347c597b4d57e977d48859e797
mumblecoder <mumblecoder@gmail.com>
2020-12-30 23:57:23 +0900

    test code 공부중

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import com.example.demo.controller.MainController;
+import com.example.demo.service.MainService;
+
+@WebMvcTest(controllers = MainController.class)
 class DemoApplicationTests {
 
+	@Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private MainService service;
+	
 	@Test
 	void contextLoads() {
 	}
---
+++ src/test/java/com/example/demo/todo/TodoTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.todo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TodoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
