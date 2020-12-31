92c64e541c6a1f095c360ebfa02eadab9a2d6f82
mumblecoder <mumblecoder@gmail.com>
2020-12-31 23:00:01 +0900

    -

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import com.example.demo.bean.ToDo;
 import com.example.demo.controller.MainController;
 import com.example.demo.service.MainService;
 
@@ -20,7 +28,24 @@ class DemoApplicationTests {
     private MainService service;
 	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		
+		ToDo todo = new ToDo();
+		todo.setContent("test");
+		mvc.perform(post("/todos", todo));
+		
+        //when
+        final ResultActions actions = mvc.perform(get("/todos", 1L)
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print());
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("content").value("test"))
+        ;
+		
+		
 	}
 
 }
